d9770438b3fac97d18565b0d39a390982aa667d5
Ali Ghodousi <48363598+masturdating1991@users.noreply.github.com>
2021-02-04 22:43:37 +0330

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,7 +13,6 @@ const Card = () => {
                 <img src={avatar_url} alt={name}/>
                 <div>
                     <h4>{name}</h4>
-                    <p>@{twitter_username || "masturdating1991"}</p>
                 </div>
                 <a href={html_url}>follow</a>
             </header>
